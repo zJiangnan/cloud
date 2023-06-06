8d1d9f7f158bf2e7349545e80580cc92584af33f
Unicorn <13669036625@163.com>
2023-06-06 22:53:53 +0800

    build(构建): echo
    构建子模块：[payment8001]

---
+++ provider-payment8001/src/main/java/cn/echo/cloud/PaymentApplication.java
@@ -0,0 +1,20 @@
+package cn.echo.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 主启动类
+ * <p></p>
+ *
+ * @author Unicorn Pink
+ * @version 1.0
+ * @createdate 2023/6/6
+ * @see PaymentApplication
+ **/
+@SpringBootApplication
+public class PaymentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApplication.class, args);
+    }
+}
